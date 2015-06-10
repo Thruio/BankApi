af1caf4fb0433a00f4cc53629531efd982db25ad
Matthew Baggett <matthew@baggett.me>
2015-06-10 11:51:31 +0100

    Read accounts.yml for selenium config..

--- get_balances.php
+++ get_balances.php
@@ -2,12 +2,26 @@
 
 require_once("bootstrap.php");
 
-$host = 'http://localhost:4444/wd/hub'; // this is the default
-$seleniumDriver = RemoteWebDriver::create($host, DesiredCapabilities::firefox());
+$settings = \Symfony\Component\Yaml\Yaml::parse(file_get_contents(APP_ROOT . "/accounts.yml"));
 
-$accounts = \Symfony\Component\Yaml\Yaml::parse(file_get_contents(APP_ROOT . "/accounts.yml"));
+$host = $settings['Selenium']['Host']; // this is the default
+if(isset($settings['Selenium']['BrowserDriver'])){
+  switch($settings['Selenium']['BrowserDriver']){
+    case 'chrome':
+      $desiredCapabilities = DesiredCapabilities::chrome();
+      break;
+    case 'firefox':
+    default:
+      $desiredCapabilities = DesiredCapabilities::firefox();
+      break;
+  }
+}else{
+  $desiredCapabilities = DesiredCapabilities::firefox();
+}
 
-foreach($accounts as $account_name => $details){
+$seleniumDriver = RemoteWebDriver::create($host, $desiredCapabilities);
+
+foreach($settings['accounts'] as $account_name => $details){
   $connectorName = "\\Thru\\Bank\\" . $details['connector'];
   $connector = new $connectorName();
   if(!$connector instanceof \Thru\Bank\BaseBankAccount){
