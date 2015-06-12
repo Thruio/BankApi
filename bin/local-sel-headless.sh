34fad75cacf8ccb1f589ab78c50a29a5e9f74a80
Matthew Baggett <matthew@baggett.me>
2015-06-12 11:58:20 +0100

    Divorce application from selenium

--- bin/local-sel-headless.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-JAVA_OPTS="$(java-dynamic-memory-opts.sh)"
-echo INFO: JAVA_OPTS are "$JAVA_OPTS"
-
-java $JAVA_OPTS \
-    -jar /opt/selenium/selenium-server-standalone.jar \
-    -port $SELENIUM_PORT
