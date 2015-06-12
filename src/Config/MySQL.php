cfa6f822d34b8915ddd723aec2669aeafbbbffd8
Matthew Baggett <matthew@baggett.me>
2015-06-12 13:02:20 +0100

    Use tutum vars

--- src/Config/MySQL.php
+++ src/Config/MySQL.php
@@ -2,11 +2,10 @@
 
 // Database Settings
 if (isset($_SERVER['MYSQL_PORT'])) {
-  $host = parse_url($_SERVER['MYSQL_PORT']);
   $database = new \Thru\ActiveRecord\DatabaseLayer(array(
     'db_type'     => 'Mysql',
-    'db_hostname' => $host['hostname'],
-    'db_port'     => $host['port'],
+    'db_hostname' => $_SERVER['MYSQL_1_ENV_TUTUM_NODE_FQDN'],
+    'db_port'     => $_SERVER['MYSQL_1_PORT_3306_TCP_PORT'],
     'db_username' => $_SERVER['THRUIO_ENV_MYSQL_USER'],
     'db_password' => $_SERVER['THRUIO_ENV_MYSQL_PASS'],
     'db_database' => $_SERVER['THRUIO_ENV_MYSQL_DATABASE'],
