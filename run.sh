ccdbfcd444b0c8631e8b0734ad676d174e4f49f2
Matthew Baggett <matthew@baggett.me>
2015-06-12 16:29:58 +0100

    Wait, apache never completes..

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+cd /app/;
+crond -n
+php get_balances.php
+
 chown www-data:www-data /app -R
 source /etc/apache2/envvars
 tail -F /var/log/apache2/* &
 exec apache2 -D FOREGROUND
 
-cd /app/;
-crond -n
-php get_balances.php
