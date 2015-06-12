11f6c9eb53f2e7b5f2379887e1c634650a57f712
Matthew Baggett <matthew@baggett.me>
2015-06-12 13:56:28 +0100

    change slack config

--- src/Config/Slack.php
+++ src/Config/Slack.php
@@ -1,5 +1,5 @@
 <?php
 
-define("SLACK_TOKEN",   "xoxb-5065709056-UfszYx3S6qVcEEAVQApfu0LG");
+define("SLACK_TOKEN",   "xoxb-6311214994-GVvhNzjRMekeWFhhbE52BkmB");
 define("SLACK_CHANNEL", "#BankApi");
-define("SLACK_USER",    "bankapi");
+define("SLACK_USER",    "bank");
