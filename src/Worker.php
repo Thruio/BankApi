5d71152be357816ed65f9e1e3ba5ca862a3629ed
Matthew Baggett <matthew@baggett.me>
2015-08-03 17:34:10 +0100

    AccountHolder guff.

--- src/Worker.php
+++ src/Worker.php
@@ -4,6 +4,10 @@ namespace Thru\Bank;
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+use Thru\BankApi\Banking\BankAccountAuthException;
+use Thru\BankApi\Banking\BaseBankAccount;
+use Thru\BankApi\Models\Account;
+use Thru\BankApi\Models\AccountHolder;
 
 class Worker{
     public function run(){
@@ -32,49 +36,52 @@ class Worker{
         }else{
           $desiredCapabilities = \DesiredCapabilities::firefox();
         }
-        
-        $seleniumDriver = \RemoteWebDriver::create($host, $desiredCapabilities);
-        
-        $monolog = new Logger("BankAPI");
-        
-        #$chromeLoggerHandler = new \Monolog\Handler\ChromePHPHandler();
-        #$chromeLoggerHandler->setFormatter(new \Monolog\Formatter\ChromePHPFormatter());
-        #$monolog->pushHandler($chromeLoggerHandler);
-        
-        $slackLoggerHandler = new \Monolog\Handler\SlackHandler(SLACK_TOKEN, SLACK_CHANNEL, SLACK_USER, null, null, \Monolog\Logger::DEBUG);
-        $slackLoggerHandler->setFormatter(new \Monolog\Formatter\LineFormatter());
-        $monolog->pushHandler($slackLoggerHandler);
-        
-        $run = new \Thru\BankApi\Models\Run();
-        $run->setLogger($monolog);
-        $run->save();
-        
-        foreach($settings['Accounts'] as $account_name => $details){
-          echo "Logging into {$account_name}...\n";
-        
-          $account = \Thru\BankApi\Models\Account::FetchOrCreateByName($account_name);
-          if(strtotime($account->last_check) >= time() - 60*60){
-            echo " > Skipping, ran less than 60 minutes ago.\n\n";
-            continue;
-          }
-          $connectorName = "\\Thru\\BankApi\\Banking\\" . $details['connector'];
-          $connector = new $connectorName($account_name);
-          if(!$connector instanceof \Thru\BankApi\Banking\BaseBankAccount){
-            throw new \Exception("Connector is not instance of BaseBankAccount");
-          }
-          $connector->setAuth($details['auth']);
-          $connector->setSelenium($seleniumDriver);
-          try {
-            $connector->run($run);
-          }catch(\Thru\BankApi\Banking\BankAccountAuthException $authException){
-            echo $authException->getMessage();
-          }
-        
-          echo "\n\n";
+        foreach($settings['People'] as $person) {
+            $accountHolder = AccountHolder::FetchOrCreateByName($person['Name']);
+            $seleniumDriver = \RemoteWebDriver::create($host, $desiredCapabilities);
+
+            $monolog = new Logger("BankAPI");
+
+            #$chromeLoggerHandler = new \Monolog\Handler\ChromePHPHandler();
+            #$chromeLoggerHandler->setFormatter(new \Monolog\Formatter\ChromePHPFormatter());
+            #$monolog->pushHandler($chromeLoggerHandler);
+
+            $slackLoggerHandler = new \Monolog\Handler\SlackHandler(SLACK_TOKEN, SLACK_CHANNEL, SLACK_USER, null, null,
+              \Monolog\Logger::DEBUG);
+            $slackLoggerHandler->setFormatter(new \Monolog\Formatter\LineFormatter());
+            $monolog->pushHandler($slackLoggerHandler);
+
+            $run = new \Thru\BankApi\Models\Run();
+            $run->setLogger($monolog);
+            $run->save();
+
+            foreach ($person['Accounts'] as $account_name => $details) {
+                echo "Logging into {$person['Name']}'s {$account_name}...\n";
+
+                $account = Account::FetchOrCreateByName($accountHolder, $account_name);
+                if (strtotime($account->last_check) >= time() - 60 * 60) {
+                    echo " > Skipping, ran less than 60 minutes ago.\n\n";
+                    continue;
+                }
+                $connectorName = "\\Thru\\BankApi\\Banking\\" . $details['connector'];
+                $connector = new $connectorName($account_name);
+                if (!$connector instanceof BaseBankAccount) {
+                    throw new \Exception("Connector is not instance of BaseBankAccount");
+                }
+                $connector->setAuth($details['auth']);
+                $connector->setSelenium($seleniumDriver);
+                try {
+                    $connector->run($accountHolder, $run);
+                } catch (BankAccountAuthException $authException) {
+                    echo $authException->getMessage();
+                }
+
+                echo "\n\n";
+            }
+
+            $seleniumDriver->close();
+
+            $run->end();
         }
-        
-        $seleniumDriver->close();
-        
-        $run->end();
     }
 }
