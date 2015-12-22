fa897f512a793ffe64835ba9a3ae3fca6f7abbe6
Matthew Baggett <matthew@baggett.me>
2015-12-22 10:40:53 +0000

    Use provided details

--- src/Config/MySQL.php
+++ src/Config/MySQL.php
@@ -1,16 +1,17 @@
 <?php
+$environment = array_merge($_ENV, $_SERVER);
 
 // Database Settings
 if (isset($_SERVER['DB_PORT'])) {
-  $host = parse_url($_SERVER['DB_PORT']);
+    $host = parse_url($environment['DB_PORT']);
 
     $database = new \Thru\ActiveRecord\DatabaseLayer(array(
     'db_type'     => 'Mysql',
     'db_hostname' => isset($host['hostname'])?$host['hostname']:$host['host'],
     'db_port'     => $host['port'],
-    'db_username' => $_SERVER['THRUIO_ENV_MYSQL_USER'],
-    'db_password' => $_SERVER['THRUIO_ENV_MYSQL_PASS'],
-    'db_database' => $_SERVER['THRUIO_ENV_MYSQL_DATABASE'],
+    'db_username' => $environment['DB_ENV_MYSQL_USER'],
+    'db_password' => $environment['DB_ENV_MYSQL_PASSWORD'],
+    'db_database' => $environment['DB_ENV_MYSQL_DATABASE'],
     ));
 } else {
   $database = new \Thru\ActiveRecord\DatabaseLayer(array(
